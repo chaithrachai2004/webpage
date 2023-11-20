6529c14d85ed6f44b11965dc46fd5b79980fbcba
chaithrachai2004 <chaithrachaip2004@gmail.com>
2023-11-20 20:12:02 +0530

    msg

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import './Footer.css'
-
+// import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 const Footer = () => {
+  const footerStyle = {
+    backgroundColor: '#1976D2',
+    color: 'white',
+    textAlign: 'center',
+  };
+
+  const iconStyle = {
+    color: 'white',
+    marginRight: '10px',
+  };
+
   return (
-    <div>
-     <p className='para'> <center><b><u><i>Footer</i></u></b></center></p>
-      </div>
-  )
+    <div style={footerStyle}>
+      <IconButton color="inherit" aria-label="Facebook" href="https://www.facebook.com">
+        <FacebookIcon style={iconStyle} />
+      </IconButton>
+      <IconButton color="inherit" aria-label="Twitter" href="https://www.twitter.com">
+        <TwitterIcon style={iconStyle} />
+        </IconButton>
+        <IconButton color="inherit" aria-label="Instagram" href="https://www.instagram.com">
+        <InstagramIcon style={iconStyle} />
+      </IconButton>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
---
+++ src/Components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from './sri.jpg'
+
+const Home = () => {
+  return (
+    <div>
+        <p><b><u><center><h1>SHRIDEVI  INSTITUTE  OF  ENGINEERING  AND  TECHNOLOGY</h1></center></u></b></p> <br/>
+        <center><img src={Image} height={200} width={200}/></center>
+      
+        <p> Shridevi Institute of Engineering and Technology (SIET), Tumakuru, Karnataka is affiliated to Visvesvaraya Technological University, Belagavi, Karnataka, and is approved by AICTE New Delhi.<br/> Shridevi Institute of Engineering and Technology is a brainchild of Honorable Dr. M. R. Hulinaykar, (Chairman and Managing Trustee, SCT, Tumakuru).<br/> He is the President of Sri Shridevi Charitable Trust (R.), which is running 12 Institutions under various streams of education. His love for education and care for his native district manifested itself in the form of this college.
+<br/>
+SIET was established in the year 2002 with the sole aim to impart quality Technical Education. It is approved by AICTE and affiliated to Visvesvaraya Technological University, Belagavi Karnataka state and presently offers undergraduate and postgraduate Programmes with annual intake of 345 and 84 respectively.
+<br/>
+At present, College offers 7 Under-Graduate Programmes, 4 Post Graduate Programmes and 4 Ph.D. Research Programmes – covering “Engineering and Technology” and “Management” streams.<br/> Since 2006, 17+ Batches of Engineering students have successfully passed out and many of our students have pursued their higher studies in India or abroad and have well settled in their life.</p>
+
+
+    </div>
+  )
+}
+
+export default Home
